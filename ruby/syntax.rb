f4e914130441bfa36a03ba71ec1b2ef60dc46d49
Patrick Gramatowski <63557278+patrickgramatowski@users.noreply.github.com>
2021-11-21 09:08:14 +0100

    Adds new syntax tricks

--- ruby/syntax.rb
+++ ruby/syntax.rb
@@ -13,3 +13,25 @@ Regexp.last_match.post_match # => "..."
 $` # => ".." same as Regexp.last_match.pre_match
 
 $' # => "..." same as Regexp.last_match.post_match
+
+# Parallel Assignment
+
+a = [1, 2, 3, 4]
+
+b, *c = 99, a # => b == 99,	c == [[1, 2, 3, 4]]
+b, c = 99, *a # => b == 99,	c == 1
+b, *c = 99, *a # => b == 99,	c == [1, 2, 3, 4]
+
+# Spaces as String Delimiters
+
+s = % text .length # => 4
+
+# Instance Variable Interpolation Shorthand
+
+@xyz = 444
+
+"Cost: #@xyz$" # => "Cost: 444$"
+
+# Exploding Ranges Into Arrays
+
+[*1..8] # => [1, 2, 3, 4, 5, 6, 7, 8]
